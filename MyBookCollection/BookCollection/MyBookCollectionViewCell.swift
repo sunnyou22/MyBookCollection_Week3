e1f4904db4570a4f9fe8d897da874dc8c4fa30e8
sunnyou22 <sunnyouyaya22@gmail.com>
2022-07-20 17:13:33 +0900

    Initial Commit

---
+++ MyBookCollection/BookCollection/MyBookCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyBookCollectionViewCell.swift
+//  MyBookCollection
+//
+//  Created by 방선우 on 2022/07/20.
+//
+
+import UIKit
+
+class MyBookCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bookCover: UIImageView!
+    @IBOutlet weak var gradeLabel: UILabel!
+}
---
+++ MyBookCollection/BookCollection/MyBookCollectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MyBookCollectionViewController.swift
+//  MyBookCollection
+//
+//  Created by 방선우 on 2022/07/20.
+//
+
+import UIKit
+
+class MyBookCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+    }
+}
