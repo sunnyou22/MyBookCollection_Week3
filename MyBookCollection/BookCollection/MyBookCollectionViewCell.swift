0236d44aaf7d2ea591e02afb3bb87d559b22d445
sunnyou22 <sunnyouyaya22@gmail.com>
2022-07-20 19:49:53 +0900

    feat: Toast라이브러리 추가 - 해당 셀을 클릭하면 책 제목이 나옴

--- MyBookCollection/BookCollection/MyBookCollectionViewCell.swift
+++ MyBookCollection/BookCollection/MyBookCollectionViewCell.swift
@@ -13,19 +13,19 @@ class MyBookCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var bookImage: UIImageView!
     @IBOutlet weak var gradeLabel: UILabel!
-
-
-func configureCell(data: Book) {
-    let backgroundColor: [UIColor] = [.gray, .blue, .orange, .brown, .purple]
     
-    view.clipsToBounds = true
-    view.layer.cornerRadius = 20
-    view.backgroundColor = backgroundColor[Int.random(in: 0...backgroundColor.count - 1)]
-    titleLabel.text = data.title
-    titleLabel.font = .boldSystemFont(ofSize: 15)
-    titleLabel.textColor = .systemBackground
-    gradeLabel.font = .systemFont(ofSize: 13)
-    gradeLabel.textColor = .systemBackground
     
-}
+    func configureCell(data: Book) {
+        let backgroundColor: [UIColor] = [.gray, .blue, .orange, .brown, .purple]
+        
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 20
+        // 겹치는 색이 너무 많다 안겹치게할 수 있는 방법이 있을까
+        view.backgroundColor = backgroundColor[Int.random(in: 0...backgroundColor.count - 1)]
+        titleLabel.text = data.title
+        titleLabel.font = .boldSystemFont(ofSize: 15)
+        titleLabel.textColor = .systemBackground
+        gradeLabel.font = .systemFont(ofSize: 13)
+        gradeLabel.textColor = .systemBackground
+    }
 }
--- MyBookCollection/BookCollection/MyBookCollectionViewController.swift
+++ MyBookCollection/BookCollection/MyBookCollectionViewController.swift
@@ -12,8 +12,10 @@
 
 import UIKit
 import Kingfisher
-class MyBookCollectionViewController: UICollectionViewController {
+import Toast
 
+class MyBookCollectionViewController: UICollectionViewController {
+    
     override func viewDidLoad() {
         let layout = UICollectionViewFlowLayout()
         let spacing: CGFloat = 8
@@ -39,11 +41,20 @@ class MyBookCollectionViewController: UICollectionViewController {
         let url = URL(string: BookInfo().books[indexPath.row].cover)
         let cellIndex = BookInfo().books[indexPath.row]
         
-//        cell.titleLabel.text = cellIndex.title -> configureCell로 쓴다면?
+//        cell.titleLabel.text = cellIndex.title -> configureCell로 쓴다면? 아래코드
         cell.configureCell(data: cellIndex)
         cell.bookImage.kf.setImage(with: url) // import까먹지 말자
         cell.gradeLabel.text = "\(cellIndex.grade)"
-        
+
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyBookCollectionViewCell", for: indexPath) as! MyBookCollectionViewCell
+        
+        // 이걸 불러오지 않으면 스토리보드상의 레이블이 나옴
+        // 함수는 호출하고 실행하면 메모리에서 해제되기 때문일까
+        cell.configureCell(data: BookInfo().books[indexPath.row])
+        self.view.makeToast("\(cell.titleLabel.text!)로 이동합니다", duration: 2, position: .center)
     }
 }
