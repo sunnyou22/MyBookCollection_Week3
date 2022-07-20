0c3535252651c07254cb76cf4d63ae1e0400f498
sunnyou22 <sunnyouyaya22@gmail.com>
2022-07-20 19:28:01 +0900

    feat: 셀의 모서리과 색 지정

--- MyBookCollection/BookCollection/MyBookCollectionViewCell.swift
+++ MyBookCollection/BookCollection/MyBookCollectionViewCell.swift
@@ -9,12 +9,18 @@ import UIKit
 
 class MyBookCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var view: UIView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var bookImage: UIImageView!
     @IBOutlet weak var gradeLabel: UILabel!
 
 
 func configureCell(data: Book) {
+    let backgroundColor: [UIColor] = [.gray, .blue, .orange, .brown, .purple]
+    
+    view.clipsToBounds = true
+    view.layer.cornerRadius = 20
+    view.backgroundColor = backgroundColor[Int.random(in: 0...backgroundColor.count - 1)]
     titleLabel.text = data.title
     titleLabel.font = .boldSystemFont(ofSize: 15)
     titleLabel.textColor = .systemBackground
